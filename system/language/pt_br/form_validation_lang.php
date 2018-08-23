8a68e65d6926426f74042b0131d8360d59f8eccd
edeniltondj <edeniltonmichael.dj@gmail.com>
2018-08-23 09:07:24 -0300

    Adicionado commit da IDE integrda ao git

---
+++ system/language/pt_br/form_validation_lang.php
@@ -0,0 +1,42 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['form_validation_required']					= 'este campo é obrigatório';
+$lang['form_validation_isset']						= 'precisa ser preenchido';
+$lang['form_validation_valid_email']				= 'deve ser um email válido';
+$lang['form_validation_valid_emails']				= 'devem ser emails válidos';
+$lang['form_validation_valid_url']					= 'deve ser um URL válido';
+$lang['form_validation_valid_ip']					= 'deve ser um IP válido';
+$lang['form_validation_min_length']					= 'deve conter pelo menos {param} caracteres.';
+$lang['form_validation_max_length']					= 'não pode possuir mais que {param} caracteres.';
+$lang['form_validation_exact_length']				= 'deve conter exatamente {param} caracteres.';
+$lang['form_validation_alpha']						= 'deve conter somente letras(acentos não permitidos).';
+$lang['form_validation_alpha_numeric']				= 'deve conter somente letras(acentos não permitidos) e numeros.';
+$lang['form_validation_alpha_numeric_url']			= 'deve conter somente letras(acentos não permitidos),numeros, traços(-) e underscores(_).';
+$lang['form_validation_alpha_numeric_spaces']		= 'deve conter somente letras(acentos não permitidos), numeros e espaços.';
+$lang['form_validation_alpha_pt_br']				= 'deve conter somente letras';
+$lang['form_validation_alpha_pt_br_spaces']			= 'deve conter somente letras e espaços';
+$lang['form_validation_alphanumeric_pt_br']			= 'deve conter somente letras e numeros';
+$lang['form_validation_alphanumeric_pt_br_spaces']	= 'deve conter somente letras, numeros e espaços';
+$lang['form_validation_alpha_dash']					= 'deve conter somente letras(acentos não permitidos), numeros, underlines(_), e barras(/)';
+$lang['form_validation_numeric']					= 'deve conter somente numeros';
+$lang['form_validation_is_numeric']					= 'deve conter apenas valores numericos';
+$lang['form_validation_integer']					= 'deve conter apenas numeros inteiros';
+$lang['form_validation_regex_match']				= 'está em formato incorreto';
+$lang['form_validation_matches']					= 'deve ser igual ao campo {param}';
+$lang['form_validation_differs']					= 'deve ser diferente do campo {param}';
+$lang['form_validation_is_unique'] 					= 'este dado não pode se repetir no banco de dados';
+$lang['form_validation_is_natural']					= 'deve conter apenas numeros naturais';
+$lang['form_validation_is_natural_no_zero']			= 'deve conter apenas numeros naturais maiores que zero(0)';
+$lang['form_validation_decimal']					= 'deve ser um número com casas decimais';
+$lang['form_validation_less_than']					= 'deve ser um número menor que {param}.';
+$lang['form_validation_less_than_equal_to']			= 'deve ser um número menor ou igual a {param}.';
+$lang['form_validation_greater_than']				= 'deve ser um número maior que {param}.';
+$lang['form_validation_greater_than_equal_to']		= 'deve ser um numero maior ou igual a {param}.';
+$lang['form_validation_valid_cpf']					= 'O CPF {param} informado não é válido.';
+$lang['form_validation_valid_cep']					= 'O CEP {param} informado não é válido.';
+$lang['form_validation_valid_br_date'] = 'deve estar no formato (dd/mm/aaaa)';
+$lang['form_validation_is_equal_f'] = 'Esse campo deve ser igual ao campo {param}';
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/english/form_validation_lang.php */
