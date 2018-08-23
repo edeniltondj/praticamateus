72747e6008fd596f9102faf0b3c8775e605ecd2b
edeniltondj <edeniltonmichael.dj@gmail.com>
2018-08-23 12:35:55 -0300

    Listagem Geral de Estoque

--- application/models/Estoque_model.php
+++ application/models/Estoque_model.php
@@ -11,6 +11,27 @@ class Estoque_model extends Extended_model{
     
     public function get_por_filial($filial){
         
+        if( !empty($filial) ){
+            $this->db->where('filial',$filial);
+        }
+        
+        $produtos = $this->db->get('produto')->result_array();
+        
+        for($i=0;$i< count($produtos);$i++){
+            $produto = $produtos[$i];
+            $this->db->where('produto_id',$produto['id']);
+            $estoque = $this->db->get('estoque')->row_array();
+            
+            $quantidade_disp = 0;
+            
+            if( isset($estoque['quantidade']) ){
+                $quantidade_disp = $estoque['quantidade'] - $produto['reservado'];
+            }
+            
+            $produtos[$i]['quantidade_disponivel'] = $quantidade_disp;
+        }
+        
+        return $produtos;
     }
     
     
